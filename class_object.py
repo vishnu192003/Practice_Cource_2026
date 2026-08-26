00290594b31250a77094a84ea208397611d343dd
vishnu192003 <chacherkarvishnu@gmail.com>
2026-08-26 19:18:56 +0530

    changes applied class

--- class_object.py
+++ class_object.py
@@ -1,5 +1,6 @@
 class first:  #iterative statement(transfer condition ) break,pass ,continue 
-    #constructor
+    #constructor --> invisible default function constructor
+    #init method 
     name='vishnu'
     age= 23
     add= 'nagpur'
@@ -10,4 +11,24 @@ class first:  #iterative statement(transfer condition ) break,pass ,continue
 obharsh = first()
 #print(obharsh)
 
+
+#class
+#cl is the collection of object it is not a real world entity 
+#it just a templet or blueprint or prototype.
+
+# object
+# object is an isinstance of a class  
+# it occupied a memory
+
+# function ---> method
+
+class first:
+    def __init__(self):
+        self.name = "srushti"
+        self.age = 23
+        self.add = "Nagpur"
+        print("name of student is :", self.name)
+        print("age of student is :", self.age)
+
+obvishun = first()
 
