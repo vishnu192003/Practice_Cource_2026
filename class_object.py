9e86136ee548f586cf23ab136c48e3e92b96b9eb
vishnu192003 <chacherkarvishnu@gmail.com>
2026-08-27 15:19:17 +0530

    examples

--- class_object.py
+++ class_object.py
@@ -78,4 +78,45 @@ vi =Second("harsh",19,"hyd")
 
 
 
-# 2  class create karna hai ()
+# 2 Classes create karne ka example:
+
+# Class 1: Student
+class Student:
+    def __init__(self, name, roll_no, course):
+        self.name = name
+        self.roll_no = roll_no
+        self.course = course
+
+    def display_details(self):
+        print("----- Student Details -----")
+        print("Name   :", self.name)
+        print("Roll No:", self.roll_no)
+        print("Course :", self.course)
+
+
+# Class 2: Teacher
+class Teacher:
+    def __init__(self, name, subject, salary):
+        self.name = name
+        self.subject = subject
+        self.salary = salary
+
+    def display_details(self):
+        print("----- Teacher Details -----")
+        print("Name   :", self.name)
+        print("Subject:", self.subject)
+        print("Salary :", self.salary)
+
+
+# Objects create karna:
+# 1. Student class ke objects
+student1 = Student("Vishnu", 101, "Python Full Stack")
+student2 = Student("Sarthak", 102, "Data Science")
+
+# 2. Teacher class ke objects
+teacher1 = Teacher("Dr. Sharma", "Computer Science", 50000)
+
+# Methods call karna:
+student1.display_details()
+student2.display_details()
+teacher1.display_details()
