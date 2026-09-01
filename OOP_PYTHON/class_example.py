7e72283f7bc03d82f1cf69d8e594adf32b84833d
vishnu192003 <chacherkarvishnu@gmail.com>
2026-09-01 16:48:04 +0530

    git pulling

--- OOP_PYTHON/class_example.py
+++ OOP_PYTHON/class_example.py
@@ -1,41 +1,41 @@
-# print("!!! 1.EXAMPLE  !!!")
-# print()
-# class Student:
-#     def __init__(self, fullname, age, add):
-#         self.name = fullname
-#         self.age = age
-#         self.add = add
-#         print("Student (CLASS)")
+print("!!! 1.EXAMPLE  !!!")
+print()
+class Student:
+    def __init__(self, fullname, age, add):
+        self.name = fullname
+        self.age = age
+        self.add = add
+        print("Student (CLASS)")
 
-# s1 = Student("Sarthak Joshi", 23, "ngp")
-# print("neme :",s1.name)
-# print("age :", s1.age)
-# print("add :", s1.add)
+s1 = Student("Sarthak Joshi", 23, "ngp")
+print("neme :",s1.name)
+print("age :", s1.age)
+print("add :", s1.add)
 
-# print("-" * 20)
+print("-" * 20)
 
-# s2 = Student("Harsh Kohale", 19, "del")
-# print("neme :", s2.name)
-# print("age :", s2.age)
-# print("add :", s2.add)
+s2 = Student("Harsh Kohale", 19, "del")
+print("neme :", s2.name)
+print("age :", s2.age)
+print("add :", s2.add)
 
-# print()
-# print("!!! 2 EXAMPLE !!! ")
+print()
+print("!!! 2 EXAMPLE !!! ")
 
-# class Second:
-#     def __init__(self, name, age, city):
-#         self.fullname = name
-#         self.sage = age
-#         self.scity = city
+class Second:
+    def __init__(self, name, age, city):
+        self.fullname = name
+        self.sage = age
+        self.scity = city
 
-#         print("name of student is", name)
-#         print("age of student is", age)
-#         print("city of student is", city)
+        print("name of student is", name)
+        print("age of student is", age)
+        print("city of student is", city)
 
 
-# vi = Second("vishnu", 23, "Nagpur")
-# vi = Second("sarthak", 23, "pune")
-# vi = Second("harsh", 19, "hyd")
+vi = Second("vishnu", 23, "Nagpur")
+vi = Second("sarthak", 23, "pune")
+vi = Second("harsh", 19, "hyd")
 
 
 # Fruits Class Example
